c8d3ee39c39b6d8f2b0235c571117b1212d537fd
Faizan Ghani <faizanghani2222@gmail.com>
2023-01-19 03:52:05 +0530

    comleted Assignment

--- src/User/User.model.js
+++ src/User/User.model.js
@@ -4,8 +4,8 @@ const userSchema=new mongoose.Schema({
     first_name:{type:String,required:true},
     last_name:{type:String},
     id:{type:Number,required:true},
-    tasks:{type:Array,default:[]},
-    columns:{type:Array,default:["Todo","In-ProgressEvent","Completed"]},
+    tasks:{type:Array,default:[{title:"You can manage the board with @flamecloudTrelloBot from Telegram",status:"Todo"}]},
+    columns:{type:Array,default:["Todo","In-Progress","Completed"]},
 })
 
 const User=mongoose.model("user",userSchema)
